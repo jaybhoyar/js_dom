b0d73d6cf2374f04031a2ea15fb048a28d963c40
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-01-05 10:11:54 +0530

    events 2 done

--- assignments/events/1. Practice/index.js
+++ assignments/events/1. Practice/index.js
@@ -1,42 +1,26 @@
 //Select the section with an id of container without using querySelector.
-
+document.getElementById("container");
 
 //Select the section with an id of container using querySelector.
-
+document.querySelector("#container");
 
 //Select all of the list items with a class of "second".
-
-
+document.querySelectorAll(".second");
 
 //Select a list item with a class of third, but only the list item inside of the ol tag.
 
-
-
 //Give the section with an id of container the text "Hello!".
-
-
 
 //Add the class main to the div with a class of footer.
 
-
-
 //Remove the class main on the div with a class of footer.
-
 
 //Create a new li element.
 
-
-
 //Give the li the text "four".
-
-
 
 //Append the li to the ul element.
 
-
-
 //Loop over all of the list inside the ol tag and give them a background color of "green".
-
-
 
 //Remove the div with a class of footer.
--- assignments/events/2. backgroundChange/todo.md
+++ assignments/events/2. backgroundChange/todo.md
@@ -2,5 +2,5 @@
 
 Make a webpage where when you click the background of the page should change to some different color.
 
-* Bonus
-Make a function to randomly generate the background color using combination of hex code.
+-   Bonus
+    Make a function to randomly generate the background color using combination of hex code.
